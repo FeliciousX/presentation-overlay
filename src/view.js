ab94bb0c072041aa1b8876ae73fce2cbfaf79f65
FeliciousX <feliciousx@gmail.com>
2017-02-02 20:32:55 +0800

    feat(view): add fake layout

--- src/view.js
+++ src/view.js
@@ -1,5 +1,10 @@
-import {h} from '@cycle/dom'
+import {div, section} from '@cycle/dom'
 
-export default function view (state) {
-  return h('h1', {}, 'Hello World')
-}
+export default (state) =>
+  div({ props: { id: 'wrapper' } }, [
+    div({ props: { id: 'topbar' } }, 'Title'),
+    div({ props: { id: 'content' } }, [
+      section('Hello World')
+    ]),
+    div({ props: { id: 'footer' } }, 'Footer')
+  ])
