d112f4ef8ed10e9e75614249453c6fbc803bfbb1
FeliciousX <feliciousx@gmail.com>
2017-02-02 21:44:18 +0800

    feat(view): add transition to height for content and topbar

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ function intent (sources) {
 function model (action$) {
   const initialState = {
     controls: true,
+    viewportHeight: 0,
     topbar: { style: { height: 60 } },
     content: { style: { height: 0 } },
     footer: { style: { height: 214 } }
@@ -36,14 +37,15 @@ function model (action$) {
   const viewportChange$ = action$
     .filter(propEq('type', 'viewport-change'))
     .map(prop('payload'))
-    .map(height => function (state) {
+    .map(viewportHeight => function (state) {
       return {
         ...state,
+        viewportHeight,
         content: {
           ...state.content,
           style: {
             ...state.content.style,
-            height: state.controls ? height - state.footer.style.height : height
+            height: state.controls ? viewportHeight - state.footer.style.height : viewportHeight
           }
         }
       }
--- src/view.js
+++ src/view.js
@@ -4,16 +4,25 @@ export default ({topbar, content, footer}) =>
   div({ props: { id: 'wrapper' } }, [
     div({
       props: { id: 'topbar' },
-      style: { height: `${topbar.style.height}px` }
+      style: {
+        ...topbar.style,
+        height: `${topbar.style.height}px`
+      }
     }, 'Title'),
     div({
       props: { id: 'content' },
-      style: { height: `${content.style.height}px` }
+      style: {
+        ...content.style,
+        height: `${content.style.height}px`
+      }
     }, [
       section('Hello World')
     ]),
     div({
       props: { id: 'footer' },
-      style: { height: `${footer.style.height}px` }
+      style: {
+        ...footer.style,
+        height: `${footer.style.height}px`
+      }
     }, 'Footer')
   ])
