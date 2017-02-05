cfdd291b1dc4479d8235db9308fed529afb9110f
FeliciousX <feliciousx@gmail.com>
2017-02-05 22:07:01 +0800

    chore(gh-pages): update gh-deploy script

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,27 @@
-const { echo } = require('shelljs')
+const { echo, which, exit, exec, cp } = require('shelljs')
 
-echo('hello world!')
+if (!which('git')) {
+  echo('Sorry, this script requires git')
+  exit(1)
+}
+
+if (exec('npm run build').code !== 0) {
+  echo('Error: build failed')
+  exit(1)
+}
+
+if (exec('git checkout gh-pages').code !== 0) {
+  echo('Error: git checkout failed')
+  exit(1)
+}
+
+cp('dist/index.html', 'index.html')
+cp('dist/bundle.js', 'bundle.js')
+
+exec('git add index.html bundle.js')
+if (exec('git commit -am "deploy auto-commit"').code !== 0) {
+  echo('Error: git commit failed')
+  exit(1)
+}
+
+exec('git push origin gh-pages')
