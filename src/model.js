be2b6f08036d57a8eb0d02f6b5bf92c00b0d594a
FeliciousX <feliciousx@gmail.com>
2017-02-06 03:07:50 +0800

    feat(model): implement immutablejs

--- src/model.js
+++ src/model.js
@@ -1,61 +1,35 @@
 import xs from 'xstream'
+
+import Immutable from 'immutable'
 import {propEq, prop} from 'ramda'
 
 export default function model (action$) {
-  const initialState = {
+  const initialState = Immutable.fromJS({
     controls: true,
     viewportHeight: 0,
     topbar: { height: 60, style: { marginTop: 0 } },
     content: { style: { height: 0 } },
     footer: { style: { height: 214 } }
-  }
+  })
 
   const viewportChange$ = action$
     .filter(propEq('type', 'viewport-change'))
     .map(prop('payload'))
     .map(viewportHeight => function (state) {
-      return {
-        ...state,
-        viewportHeight,
-        controls: true,
-        topbar: {
-          ...state.topbar,
-          style: {
-            ...state.topbar.style,
-            marginTop: 0
-          }
-        },
-        content: {
-          ...state.content,
-          style: {
-            ...state.content.style,
-            height: viewportHeight - state.footer.style.height - state.topbar.height
-          }
-        }
-      }
+      return state.set('viewportHeight', viewportHeight)
+        .set('controls', true)
+        .setIn(['topbar', 'style', 'marginTop'], 0)
+        .updateIn(['content', 'style', 'height'], height =>
+          viewportHeight - state.getIn(['footer', 'style', 'height']) - state.getIn(['topbar', 'height'])
+        )
     })
 
   const hideControl$ = action$
     .filter(propEq('type', 'content-mouseenter'))
     .mapTo(function (state) {
-      return {
-        ...state,
-        controls: false,
-        topbar: {
-          ...state.topbar,
-          style: {
-            ...state.topbar.style,
-            marginTop: -state.topbar.height
-          }
-        },
-        content: {
-          ...state.content,
-          style: {
-            ...state.content.style,
-            height: state.viewportHeight
-          }
-        }
-      }
+      return state.set('controls', false)
+        .updateIn(['topbar', 'style', 'marginTop'], marginTop => -state.getIn(['topbar', 'height']))
+        .setIn(['content', 'style', 'height'], state.get('viewportHeight'))
     })
 
   const showControl$ = action$
@@ -63,24 +37,11 @@ export default function model (action$) {
     .map(prop('payload'))
     .filter(ev => ev.clientY > 400)
     .mapTo(function (state) {
-      return {
-        ...state,
-        controls: true,
-        topbar: {
-          ...state.topbar,
-          style: {
-            ...state.topbar.style,
-            marginTop: 0
-          }
-        },
-        content: {
-          ...state.content,
-          style: {
-            ...state.content.style,
-            height: state.viewportHeight - state.footer.style.height - state.topbar.height
-          }
-        }
-      }
+      return state.set('controls', true)
+        .setIn(['topbar', 'style', 'marginTop'], 0)
+        .updateIn(['content', 'style', 'height'], height =>
+          state.get('viewportHeight') - state.getIn(['footer', 'style', 'height']) - state.getIn(['topbar', 'height'])
+        )
     })
 
   const reducer$ = xs.merge(
@@ -90,5 +51,6 @@ export default function model (action$) {
   )
 
   return reducer$.fold((state, reducer) => reducer(state), initialState)
+    .map(m => m.toJS())
     .drop(1)
 }
