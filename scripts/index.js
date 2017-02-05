baf90295e83984cff7ebbb69aafbbc8fdd0e21ab
FeliciousX <feliciousx@gmail.com>
2017-02-05 23:00:02 +0800

    chore(gh-pages): improve gh-deploy script to use async on build

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,36 @@
-const { echo, which, exit, exec, cp } = require('shelljs')
+const { which, exit, exec, cp } = require('shelljs')
 
 if (!which('git')) {
-  echo('Sorry, this script requires git')
+  console.error('Sorry, this script requires git')
   exit(1)
 }
 
-if (exec('npm run build').code !== 0) {
-  echo('Error: build failed')
-  exit(1)
-}
+console.log('Building with webpack...')
 
-if (exec('git checkout gh-pages').code !== 0) {
-  echo('Error: git checkout failed')
-  exit(1)
-}
+exec('npm run build', { asnyc: true, silent: true }, (code, stdout, stderr) => {
+  if (code !== 0) {
+    console.error(stderr)
+    exit(1)
+  }
 
-cp('dist/index.html', 'index.html')
-cp('dist/bundle.js', 'bundle.js')
+  const commit = exec('git log --oneline -1', { silent: true })
+  if (exec('git checkout gh-pages').code !== 0) {
+    console.error('Error: git checkout failed')
+    exit(1)
+  }
 
-exec('git add index.html bundle.js')
-if (exec('git commit -am "deploy auto-commit"').code !== 0) {
-  echo('Error: git commit failed')
-  exit(1)
-}
+  cp('dist/index.html', 'index.html')
+  cp('dist/bundle.js', 'bundle.js')
 
-exec('git push origin gh-pages')
+  exec('git add index.html bundle.js')
+  if (exec(`git commit -am "deploys ${commit}"`).code !== 0) {
+    console.error('Error: git commit failed')
+    exit(1)
+  }
+
+  if (exec('git push origin gh-pages').code !== 0) {
+    console.error('Error: git push to origin failed')
+    exit(1)
+  }
+  exec('git checkout master')
+})
